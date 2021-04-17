0088fc6e7f418a42b18d032ddc7ba9fce0867a72
Alex <alexjianghuiting@gmail.com>
2021-04-17 19:14:41 +0800

    test-1

--- EPC.py
+++ EPC.py
@@ -4,9 +4,10 @@ import os
 dirfile = os.path.abspath('') # code的文件位置，我默认将其存放在根目录下
 repo = Repo(dirfile)
 
-for i in range(500):
+for i in range(2):
+    os.system("echo 1 >> \"a.txt\"")
     g = repo.git
     g.add("--all")
-    g.commit("-m ")
+    g.commit("-m test-1")
     g.push()
     print("Successful push!")
